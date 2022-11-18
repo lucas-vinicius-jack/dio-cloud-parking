9049fa1b3a23fe964f0c2b2160de243bfb56f18b
Lucas Vinicius <lucasvinicius1657@gmail.com>
2022-11-18 03:24:24 -0300

    adiciona swagger para testes

--- src/main/java/one/digitalinnovation/cloudparking/controller/HelloControler.java
+++ src/main/java/one/digitalinnovation/cloudparking/controller/HelloControler.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("/")
+@ApiIgnore
 public class HelloControler {
 
     @GetMapping
